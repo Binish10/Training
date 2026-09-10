fc3697a425cc09cec16a22b7d5e9a779ce79de8b
binish <160550040+Binish10@users.noreply.github.com>
2026-09-10 12:45:55 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Bump C on EVERY change to index.html, or installed copies keep serving the old shell. */
-const C = 'training-v2';
+const C = 'training-v3';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
